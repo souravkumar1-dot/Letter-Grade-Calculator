aed124e236b81b1b0ecfd32c2da6d3d5d621b84a
Sourav Kumar <souravkumarsingh077@gmail.com>
2026-07-14 19:39:16 +0530

    Add files via upload

---
+++ LetterGradeCalculator.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+
+public class LetterGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else {
+            System.out.println("F");
+        }
+
+        sc.close();
+    }
+}
